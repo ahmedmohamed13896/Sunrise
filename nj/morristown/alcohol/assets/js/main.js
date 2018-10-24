89daf9952fc96e19d17a16dcd59a7e19ff06a3d1
ahmed mohamed <ahmedmohamed77765@hotmail.com>
2018-10-24 04:54:02 +0200

    added more pages
    i have made more pages in alcohol,opiates,benzos and heroin work.

--- nj/morristown/alcohol/assets/js/main.js
+++ nj/morristown/alcohol/assets/js/main.js
@@ -124,8 +124,16 @@ $(function(){
         slideScroll = -(slideImageWidth * 13);
     });
     
-    
-    
+    /*accordion*/
+    $('.accordion .head-body:first').css("display","block");
+    $('.accordion .h3').click(function(){
+       
+        $('.accordion .h3').removeClass('active');
+        $(this).addClass('active');
+        $(this).next().slideToggle(500);
+       $('.accordion .head-body').not($(this).next()).slideUp(500);
+         
+    });
     
     
 });
--- nj/morristown/assets/js/main.js
+++ nj/morristown/assets/js/main.js
@@ -1,8 +1,11 @@
 $(function(){
-   
-    $('.accordion .head-body:first').css("display","block");
+   /*accordion*/
+ $('.accordion .head-body:first').css("display","block");
     $('.accordion .h3').click(function(){
-       $(this).next().slideToggle(500);
+       
+        $('.accordion .h3').removeClass('active');
+        $(this).addClass('active');
+        $(this).next().slideToggle(500);
        $('.accordion .head-body').not($(this).next()).slideUp(500);
          
     });
